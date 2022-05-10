c7bb9be54eaf0d70a8077b1171dee74a419e16a2
Adrian Tetłak <93789307+S0ffen@users.noreply.github.com>
2022-05-10 22:17:57 +0200

    Add files via upload

---
+++ PracticePython.org/Exercise1.py
@@ -0,0 +1,6 @@
+name = str(input("Hello what is your name: "))
+age = int(input("What is your age:"))
+
+Age_in_100years = (2022 - age) + 100 
+print("Hello "+ name, "Your age will 100 in " + str(Age_in_100years))
+
---
+++ PracticePython.org/Exercise2.py
@@ -0,0 +1,12 @@
+number = int(input("What is your number: "))
+if number % 2 == 0:
+    print("you picked a even number")
+elif number % 2 == 1:
+    print("you picked a odd number")
+
+"""
+Extra 1
+"""
+
+if number % 4 == 0:
+    print("Your number is even and it can be divided 4")
