ce3021989119e6b7b9b092e353b4d656eaf069a4
Adrian Tetłak <93789307+S0ffen@users.noreply.github.com>
2022-05-11 23:28:26 +0200

    Solution to Exercise 4

---
+++ PracticePython.org/Exercise4.py
@@ -0,0 +1,11 @@
+from re import X
+
+
+number = int(input("Give me a number: "))
+x = range(1,number+1)
+divine = []
+for elem in x:
+    if number % elem == 0:
+        divine.append(elem)
+
+print(divine)
